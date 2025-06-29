01478784c109530791bd931d9d2961fbcdf3f67e
dustein <steindu@gmail.com>
2025-06-29 20:09:38 -0300

    065 em andamento

--- 03_news/src/pages/index.tsx
+++ 03_news/src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
+      <>
+    <Head>
+        <title>Inicio NEWS App Next</title>
+    </Head>
+
     <h1 className={styles.title}>Hello World</h1>
+      </>
   );
 }
