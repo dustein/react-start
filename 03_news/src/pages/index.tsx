eae415475540ef6f9bafa63ab4fa66f54955d404
dustein <steindu@gmail.com>
2025-06-30 14:35:03 -0300

    068 home ok

--- 03_news/src/pages/index.tsx
+++ 03_news/src/pages/index.tsx
@@ -1,13 +1,21 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
       <>
     <Head>
-        <title>Inicio NEWS App Next</title>
+        <title>Home | NEWS App NextJS</title>
     </Head>
 
-    <h1>Hello World</h1>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👻 Hey, Welcome!</span>
+        <h1>NEWS about <span>Learning React</span> trip.</h1>
+        <p>Acesse o conteúdo da jornada <br /><span>por $1.99 month!</span></p>
+      </section>
+      <img className={styles.avatar} src="./images/news-worker.svg" alt="Figure coding" />
+    </main>
       </>
   );
 }
